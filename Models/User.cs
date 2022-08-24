f633b7877cd9bfb77733fc8e57e45fc0fa3177c3
Ramadoss Elumalai <ramadosse2264@gmail.com>
2022-08-24 15:09:49 -0400

    added complex vaidation logic

--- Models/User.cs
+++ Models/User.cs
@@ -11,6 +11,13 @@ namespace FluentValidationDemo.Models
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public string Address { get; set; }
-        public string MemberShip { get; set; }
+        public MemeberShip[] MemberShips { get; set; }
+    }
+
+    public class MemeberShip
+    {
+        public string Name { get; set; }
     }
 }
+
+
--- Validators/UserValidator.cs
+++ Validators/UserValidator.cs
@@ -7,10 +7,56 @@ namespace FluentValidationDemo.Validators
     {
         public UserValidator()
         {
+            // we can use validator like this below, in case of if your having limited validation
+            // without having large validation
+            #region
+            //RuleFor(u => u.Name).NotNull().NotEmpty().WithMessage("Name is required");
+            //RuleFor(u => u.Email).NotNull().NotEmpty().WithMessage("Email is required")
+            //    .EmailAddress().WithMessage("Please provide valid email address");
+            //RuleFor(u => u.Address).NotNull().NotEmpty().WithMessage("Address is required")
+            //    .MaximumLength(10).WithMessage("Address charecter lenght should not be more then 10 Charecters");
+            //RuleFor(u => u.Address)
+            //    .Must(a => a?.ToLower().Contains("street") == true)
+            //    .WithMessage("Address must contain street");
+            //RuleForEach(u => u.MemberShips)
+            //    .SetValidator(new MemeberShipValidator());
+            #endregion
+
+            // In case if your having large validation, we can spilit it and we can use like below
+            Include(new SimpleValidator());
+            Include(new ComplexValidator());
+        }
+    }
+
+    public class SimpleValidator : AbstractValidator<User>
+    {
+        public SimpleValidator()
+        {
             RuleFor(u => u.Name).NotNull().NotEmpty().WithMessage("Name is required");
-            RuleFor(u => u.Email).EmailAddress();
-            RuleFor(u => u.Address).NotNull().MaximumLength(10);
-            RuleFor(u => u.Address).Must(a => a?.ToLower().Contains("street") == true);
+            RuleFor(u => u.Email).NotNull().NotEmpty().WithMessage("Email is required")
+                .EmailAddress().WithMessage("Please provide valid email address");
+        }
+    }
+
+    public class ComplexValidator : AbstractValidator<User>
+    {
+        public ComplexValidator()
+        {
+            RuleFor(u => u.Address).NotNull().NotEmpty().WithMessage("Address is required")
+                .MaximumLength(10).WithMessage("Address charecter lenght should not be more then 10 Charecters");
+            RuleFor(u => u.Address)
+                .Must(a => a?.ToLower().Contains("street") == true)
+                .WithMessage("Address must contain street");
+            RuleForEach(u => u.MemberShips)
+                .SetValidator(new MemeberShipValidator());
+        }
+    }
+
+    public class MemeberShipValidator : AbstractValidator<MemeberShip>
+    {
+        public MemeberShipValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().NotNull().WithMessage("Name is required");
         }
     }
 }
